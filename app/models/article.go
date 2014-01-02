e89e3d78966d4c7433812a99d9298eea4c437e1c
Brian Nelson <bnelson@sugarcrm.com>
2014-01-01 23:28:58 -0800

    Fix Article sorting and Posted -> posted transition

--- app/models/article.go
+++ app/models/article.go
@@ -16,7 +16,7 @@ type Article struct {
 	Id        bson.ObjectId `bson:"_id,omitempty"`
 	Author_id bson.ObjectId `bson:"Author_id"`
 	Published bool          `bson:"Published"`
-	Posted    time.Time     `bson: "Posted"`
+	Posted    time.Time     `bson:"Posted"`
 	Title     string        `bson:"Title"`
 	Body      string        `bson:"Body"`
 	Tags      []string      `bson:"Tags"`
@@ -79,7 +79,7 @@ func GetArticleById(s *mgo.Session, Id string) *Article {
 func GetArticlesByDate(s *mgo.Session, limit int) []*Article {
 	articles := []*Article{}
 	a := new(Article)
-	query := Collection(a, s).Find(nil).Sort("{'Published': 1}").Limit(limit)
+	query := Collection(a, s).Find(bson.M{"Published": true}).Sort("-Posted").Limit(limit)
 	query.All(&articles)
 
 	for _, a := range articles {
@@ -91,7 +91,7 @@ func GetArticlesByDate(s *mgo.Session, limit int) []*Article {
 func GetArticlesByTag(s *mgo.Session, t string) []*Article {
 	articles := []*Article{}
 	a := new(Article)
-	query := Collection(a, s).Find(bson.M{"Tags": t})
+	query := Collection(a, s).Find(bson.M{"Published": true, "Tags": t}).Sort("-Posted")
 	query.All(&articles)
 
 	for _, a := range articles {
